030ffcf888457a2cc98f16f3e8494ccf474ee7b2
Justin L . Rodriguez <justin.rodriguez121206@gmail.com>
2022-02-23 21:58:39 +0800

    Product and Order Update

--- api/models/Product.js
+++ api/models/Product.js
@@ -7,7 +7,7 @@ const ProductSchema = new mongoose.Schema({
         categories: {type: Array},
         size: {type: Array},
         color: {type: Array},
-        price: {type: Number},
+        price: {type: Number, required: true},
         inStock: { type: Boolean, default: true },
     }, {timestamps: true}
 );
